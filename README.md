9a92d3e21a55a3a4bbc95151ca775ef841a9ca40
microzzzf <xiyuhefeng249@163.com>
2016-09-22 23:50:44 +0800

    fixed problem that when the server process restart, the client cannot re-connect to the server, but not good enough

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 a TCP/IP server&amp;client engine based on socket
 
 TODO:
+
 1.code refactoring.
 
 2.problem that "address already in use" when the server process exit with "Ctrl+c".
@@ -12,4 +13,5 @@ TODO:
     socket_fd is used for listening and client_fd is used for send/recv, so the best way is to use the same socket_fd to listen rather than different socket_fd in different threads to listen so we can use the same port for different client connections.
 
 4.how to connect again when the server process exit and client process does not exit.
-    (done) need to modifiy the client program.
+    (done) need to modifiy the client program. 
+    4.1. not good enough : handle with signal SIGPIPE. But it will miss three messages while re-connect to the server.
--- src/TcpClient.cpp
+++ src/TcpClient.cpp
@@ -15,43 +15,56 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <string.h>
+#include <signal.h>
 
 using namespace std;
+
+int connected = 1;
+
+void reset(int)
+{
+    connected = 0;
+}
 
 void TcpClient::create()
 {
     int client_fd = 0;
-    if((client_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+
+    while(1)
     {
-        cout<<strerror(errno)<<endl;
-        exit(1);
-    }
+        if((client_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+        {
+            cout<<strerror(errno)<<endl;
+            exit(1);
+        }
 
-    const int PORT = 3333;
-    struct sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+        const int PORT = 3333;
+        struct sockaddr_in server_addr;
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_port = htons(PORT);
+        server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    if(connect(client_fd, (struct sockaddr*)(&server_addr), sizeof(server_addr)) < 0)
-    {
-        cout<<strerror(errno)<<endl;
-        exit(1);
-    }
+        if(connect(client_fd, (struct sockaddr*)(&server_addr), sizeof(server_addr)) < 0)
+        {
+            cout<<strerror(errno)<<endl;
+            exit(1);
+        }
 
-    const int BUFFER_LEN = 4096;
-    char send_buffer[BUFFER_LEN];
-    char rec_buffer[BUFFER_LEN];
-    while(fgets(send_buffer, sizeof(send_buffer), stdin) != NULL)
-    {
-        send(client_fd, send_buffer, strlen(send_buffer), 0);
-        recv(client_fd, rec_buffer, sizeof(rec_buffer), 0);
-        cout<<rec_buffer;
+        const int BUFFER_LEN = 4096;
+        char send_buffer[BUFFER_LEN];
+        char rec_buffer[BUFFER_LEN];
+        connected = 1;
 
-        memset(send_buffer, 0, sizeof(send_buffer));
-        memset(rec_buffer, 0, sizeof(rec_buffer));
-    }
+        while(fgets(send_buffer, sizeof(send_buffer), stdin) != NULL && connected)
+        {
+            send(client_fd, send_buffer, strlen(send_buffer), 0);
+            signal(SIGPIPE, reset);
+            recv(client_fd, rec_buffer, sizeof(rec_buffer), 0);
+            cout<<rec_buffer;
 
-    close(client_fd);
-
+            memset(send_buffer, 0, sizeof(send_buffer));
+            memset(rec_buffer, 0, sizeof(rec_buffer));
+        }
+        close(client_fd);
+    }   
 }
